d976ac9b3b39b2afd2ce79966264c56ec28ec195
andersblauenfeldt <andersblauenfeldt@users.noreply.github.com>
2026-04-11 23:25:10 +0000

    Fjern Tag Manager, brug GA4 direkte

--- server.js
+++ server.js
@@ -457,9 +457,9 @@ function renderLayout(req, title, content) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TR44KP88');</script>
-  <!-- End Google Tag Manager -->
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-5NMKWW600Y"></script>
+  <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-5NMKWW600Y');</script>
   <title>${siteTitle}</title>
   <meta name="description" content="${desc}">
   <meta name="author" content="Camilla Blauenfeldt">
@@ -479,9 +479,6 @@ function renderLayout(req, title, content) {
   <style>${css}</style>
 </head>
 <body>
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TR44KP88" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
   <nav>
     <a href="/" class="logo">FruB</a>
     <div class="nav-right">
